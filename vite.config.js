020db25f8560a1cb523756b11a10ba336d0390df
Shibly Sadik <madstyle2s@gmail.com>
2026-03-26 04:06:42 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // This tells Vite your files are in the root, not a src folder
+  root: './', 
 })
